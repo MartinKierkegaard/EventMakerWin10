50d77263e5cd1ed96340f7f79d21b98aa862c33d
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2016-01-25 15:30:22 +0100

    add relaycommand add navigation

---
+++ EventMakerWin10/Handler/EventHandler.cs
@@ -0,0 +1,26 @@
+﻿using EventMakerWin10.ViewModel;
+
+namespace EventMakerWin10.Handler
+{
+    public class EventHandler
+    {
+
+        private EventViewModel eventViewModel;//= new EventViewModel();
+
+        public EventHandler(EventViewModel eventViewModel)
+        {
+            //this.eventViewModel = new EventViewModel();
+            this.eventViewModel = eventViewModel;
+        }
+
+
+        /// <summary>
+        /// tilføjer ny event
+        /// </summary>
+        public void AddEvent()
+        {
+            eventViewModel.EventCatalogSingleton.AddEvent(eventViewModel.NewEvent);
+        }
+
+    }
+}
